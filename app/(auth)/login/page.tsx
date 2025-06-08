9edf465bba8b53cb8a54d6237475313ec870a6cd
Donovan Jabbar <djabbar51@gmail.com>
2025-06-08 12:55:57 -0600

    chore: protect private routes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { SignIn } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 const page = () => {
   return (
-    <div>
-      <SignIn />
+    <div className='h-screen flex items-center justify-center'>
+      <SignIn appearance={{
+        baseTheme: dark
+      }}/>
 
     </div>
   )
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { SignUp } from '@clerk/nextjs'
-
+import React from 'react';
+import { SignUp } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 
 const page = () => {
   return (
-    <div>
-      <SignUp />
-
+    <div className='flex items-center h-full justify-center'>
+      <SignUp appearance={{ baseTheme: dark }} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
